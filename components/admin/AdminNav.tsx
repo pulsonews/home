32727129383eca792003e1b6563173b95965f4b0
pulsonews <attackdesigner2@gmail.com>
2026-08-15 20:05:44 -0300

    Add files via upload

--- components/admin/AdminNav.tsx
+++ components/admin/AdminNav.tsx
@@ -5,6 +5,7 @@ import { usePathname, useRouter } from "next/navigation";
 
 const links = [
   { href: "/admin", label: "Visão geral" },
+  { href: "/admin/noticias", label: "Notícias" },
   { href: "/admin/fontes", label: "Fontes RSS" },
   { href: "/admin/categorias", label: "Categorias" },
   { href: "/admin/banners", label: "Banners" },
